ddb3981953727398b9f857eb1a9d969dd95cd45b
kamatealif <kamatealip1@gmail.com>
2026-03-04 20:13:27 +0530

    added support for the both html and xhtml file and reading the data from it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from html.parser import HTMLParser
-
+import os 
 
 class MyHTMLParser(HTMLParser):
     def handle_data(self,data):
@@ -8,10 +8,17 @@ class MyHTMLParser(HTMLParser):
 
 def main():
     parser = MyHTMLParser()
-    with open('docs/library/csv.html','r') as file:
-        reader = file.read();
-        parser.feed(reader)
-
+    for dirpath, dirnames, filenames in os.walk('docs'):
+        for f in filenames:
+            if f.endswith('.html'):
+                print("Working with {}".format(os.path.join(dirpath,f)))
+                with open(os.path.join(dirpath,f), 'r') as html_file:
+                    parser.feed(html_file.read())
+            if f.endswith('.xhtl'):
+                print("working with {}".format(os.path.join(dirpath, f)))
+                with open(os.path.join(dirpath,f),'r') as xhtml_file:
+                    parser.feed(xhtml_file.read())
+        
 
 if __name__ == "__main__":
     main()
